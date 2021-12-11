a4c4df601dbbf37c8cee18eec0cab8171be41bdf
Hyy2001X <1804430051@qq.com>
2021-12-12 01:05:38 +0800

    AutoBuild-Actions: 20211212 功能更新
    2021/12/12 更新内容
    
    1. 新增固件标签 Default_FLAG, 一键更新固件时会自动检测带该标签的固件版本, e.g. 若固件标签为 Full, 则更新固件时仅可以更新到固件名称中带 Full 标签的版本
       补充: 用户自定义变量 Default_FLAG, 支持数字与字母的组合 [0-9a-zA-Z] 例如 [Full Lite Daily1 Test2], 不支持标点符号, 不能为空
             当该值为 AUTO 时, 将自动从配置文件名称获取, 格式为 <TARGET_PROFILE>-<FLAG>, e.g. 配置文件名称为 [x86_64-Test 或 d-team_newifi-d2-Lite], 则标签分别为 [Test Lite]; 配置文件若为不规则命名或无标签例如 [x86_64 X86 NEWIFI-D2], 则标签自动设定为 Full
    		 同时支持在手动编译时临时修改, 点击 Run workflow 后修改 固件名称后缀 [可选] 值即可, 优先级高于用户自定义变量 Default_FLAG
             固件内支持标签的快速切换, 切换方法 autoupdate --flag <标签名>, 恢复默认 autoupdate --flag reset
    2. AutoBuild_Function.sh Firmware_Diy 函数中新增 CONFIG_FILE TARGET_FLAG 等可调用变量
    3. 新增一些设备支持 (也许, 我也不知道)
    
    改动内容较大, 建议重新 Fork 本仓库

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -6,6 +6,7 @@ Firmware_Diy_Core() {
 
 	Author=AUTO
 	Author_URL=AUTO
+	Default_FLAG=AUTO
 	Default_IP="192.168.1.1"
 	Banner_Message="Powered by AutoBuild-Actions"
 
@@ -21,22 +22,24 @@ Firmware_Diy_Core() {
 
 Firmware_Diy() {
 
-	# 请在该函数内定制固件, 建议使用专业文本编辑器进行修改
+	# 请在该函数内定制固件
 
 	# 可用预设变量, 其他可用变量请参考运行日志
 	# ${OP_AUTHOR}			OpenWrt 源码作者
 	# ${OP_REPO}			OpenWrt 仓库名称
 	# ${OP_BRANCH}			OpenWrt 源码分支
-	# ${TARGET_PROFILE}		设备名称, 例如: d-team_newifi-d2
-	# ${TARGET_BOARD}		设备架构, 例如: ramips
+	# ${TARGET_PROFILE}		设备名称
+	# ${TARGET_BOARD}		设备架构
+	# ${TARGET_FLAG}		固件名称后缀
 
 	# ${Home}				OpenWrt 源码位置
-	# ${FEEDS_CONF}			[feeds.conf.default] 文件
+	# ${CONFIG_FILE}		使用的配置文件名称
+	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
 	# ${CustomFiles}		仓库中的 /CustomFiles 绝对路径
 	# ${Scripts}			仓库中的 /Scripts 绝对路径
-	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci
-	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages
-	# ${BASE_FILES}			俗称替换大法的路径, 位于 package/base-files/files
+	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci 目录
+	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
+	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
 
 	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
 	coolsnowwolf/lede:master)
@@ -48,6 +51,7 @@ Firmware_Diy() {
 		AddPackage svn other luci-app-socat Lienol/openwrt-package/trunk
 		AddPackage svn other luci-app-eqos kenzok8/openwrt-packages/trunk
 		AddPackage git other OpenClash vernesong master
+		AddPackage git other luci-app-usb3disable rufengsuixing master
 		# AddPackage git other OpenAppFilter destan19 master
 		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
 		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
@@ -57,15 +61,8 @@ Firmware_Diy() {
 			patch -i ${CustomFiles}/${TARGET_PROFILE}_mac80211.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
 			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
 			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
-			AddPackage git other luci-app-usb3disable rufengsuixing master
 		;;
 		x86_64)
-			cat >> ${Version_File} <<EOF
-
-sed -i 's#mirrors.cloud.tencent.com/lede#downloads.immortalwrt.cnsztl.eu.org#g' /etc/opkg/distfeeds.conf
-sed -i 's#18.06.9/##g' /etc/opkg/distfeeds.conf
-sed -i 's#releases/#snapshots/#g' /etc/opkg/distfeeds.conf
-EOF
 			AddPackage git other openwrt-passwall xiaorouji main
 			rm -rf packages/lean/autocore
 			AddPackage git lean autocore-modify Hyy2001X master
